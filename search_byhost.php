a297ac717772b1c356df0b82483319f484f39624
Gabor Korodi <gkorodi@gmail.com>
2017-08-12 21:31:21 +0000

    Add more missing script

---
+++ search_byhost.php
@@ -0,0 +1,140 @@
+<?php
+require_once('_includes.php');
+
+if (isset($_REQUEST['host'])) {
+  $sql = "SELECT * FROM links WHERE UPPER(link) LIKE '%".strtoupper($_REQUEST['host'])."%'".
+	  	(isset($_REQUEST['notags'])?" AND (tags IS NULL OR tags = '')":'').
+		(isset($_REQUEST['nostatus'])?' AND status != '.$_REQUEST['nostatus']:'').
+		(isset($_REQUEST['status'])?' AND status = '.$_REQUEST['status']:'').
+		' ORDER BY last_updated '.(isset($_REQUEST['olderfirst'])?' ASC':' DESC');
+  $resultset = query($sql);
+}
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php require_once('_metatags.php');?>
+	<link rel="shortcut icon" href="assets/ico/favicon.ico">
+
+	<title><?php echo APP_TITLE;?></title>
+
+	<!-- Bootstrap core CSS -->
+	<link href="assets/css/bootstrap.css" rel="stylesheet">
+
+	<!-- Custom styles for this template -->
+	<link href="assets/css/style.css" rel="stylesheet">
+
+
+	<!-- Just for debugging purposes. Don't actually copy this line! -->
+	<!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+	<script src="assets/js/modernizr.js"></script>
+</head>
+<body>
+	<?php require_once('_menu.php'); ?>
+
+	<div id="blue">
+	    <div class="container">
+			<div class="row">
+				<h3>Search Results by host <a href="http://<?php echo $_REQUEST['host'];?>" target="_newWindow"><?php echo $_REQUEST['host'];?></a>
+				</h3>
+				<form method="GET" id="frmRefine">
+					<input type="checkbox" id="fldNoTags" name="notags" <?php echo (isset($_REQUEST['notags'])?'checked':'');?>/> NoTagsOnly
+					<input type="checkbox" id="fldOlderFirst" name="olderfirst" <?php echo (isset($_REQUEST['olderfirst'])?'checked':'');?>/> OlderFirst
+					<input type="hidden" id="fldHost" name="host" value="<?php echo $_REQUEST['host'];?>" />
+				</form>
+				<small>There are <?php echo count($resultset['rows']); ?> entries</small>
+			</div><!-- /row -->
+	    </div> <!-- /container -->
+	</div><!-- /blue -->
+
+	 <div class="container mtb">
+     <div class="row">
+     <div class="col-md-12">
+			<?php
+			if (!isset($_REQUEST['host'])) {
+				?>
+				<h3>Really?</h3>
+				<p>Give me a host</p>
+				<form method="GET">
+					<input type="text" name="host" />
+					<button class="btn btn-success">Search</button>
+				</form>
+				<?php
+			} else {
+        			echo '<table class="table">';
+				foreach($resultset['rows'] AS $row) {
+          ?>
+          <tr id="row<?php echo $row[0];?>">
+            <td>
+              <b><a href="<?php echo $row[1];?>" target="_newWindow"><?php echo urldecode($row[2]);?></a></b><br />
+              <small><?php
+		      if (strpos(strtolower($row[5]), 'repair')>-1 || isset($_REQUEST['fullurl'])) {
+		      	echo $row[1];
+		      }
+              ?>
+	      
+              <?php
+              foreach(explode(',', $row[5]) AS $tag) {
+                echo '<span class="badge">'.$tag.'</span>';
+              }
+              ?>
+	      
+	      </small>
+            </td>
+            <td>
+		    <small><?php echo date('Y-m-d', strtotime($row[4]));?></small>
+            </td>
+            <td>
+              <button class="btn btn-sm btn-danger pull-right" onClick="deleteLink(<?php echo $row[0];?>);">
+                <span class="glyphicon glyphicon-remove"> </span>
+              </button>
+            </td>
+            <td>
+
+              <a class="btn btn-sm btn-info" href="linkedit.php?id=<?php echo $row[0];?>" target="_winEditLink">
+                <span class="glyphicon glyphicon-ok"> </span>
+              </a>
+            </td>
+
+          </tr>
+          <?php
+				}
+        echo '</table>';
+			}
+			?>
+    </div>
+      </div>
+	 </div><! --/container -->
+
+
+	 <?php require_once('_footer.php'); ?>
+
+	<!-- Bootstrap core JavaScript
+	================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<?php require_once('_scripts.php'); ?>
+
+	<script src="assets/js/jquery.toastmessage.js" type="text/javascript"></script>
+	<link href="assets/css/jquery.toastmessage.css" rel="stylesheet" type="text/css" />
+
+	<script>
+	$(document).ready(function(){
+		$('#fldNoTags').on('change', function() {
+			$('#frmRefine').submit();
+		});
+
+		$('#fldOlderFirst').on('change', function() {
+			$('#frmRefine').submit();
+		});
+	});
+	</script>
+
+  </body>
+</html>
---
+++ search_bytag.php
@@ -0,0 +1,171 @@
+<?php
+require_once('_includes.php');
+
+if (isset($_REQUEST['tag'])) {
+	if ($_REQUEST['tag'] == 'empty') {
+	        $sql = "SELECT * FROM links WHERE tags IS NULL ".
+	      	  	(isset($_REQUEST['notstatus'])?' AND status != '.$_REQUEST['notstatus']:'').
+	      		(isset($_REQUEST['status'])?' AND status = '.$_REQUEST['status']:'').
+		' LIMIT 300';
+
+	} else {
+	        $sql = "SELECT * FROM links WHERE UPPER(tags) LIKE '%".strtoupper($_REQUEST['tag'])."%' ".
+	      	  	(isset($_REQUEST['notstatus'])?' AND status != '.$_REQUEST['notstatus']:'').
+	      		(isset($_REQUEST['status'])?' AND status = '.$_REQUEST['status']:'').
+						' ORDER BY last_updated '.(isset($_REQUEST['dateorder'])?'ASC':'DESC');
+	}
+  $resultset = query($sql);
+}
+?><!DOCTYPE html>
+<html lang="en">
+  <head>
+	  <?php require_once('_metatags.php');?>
+    <link rel="shortcut icon" href="assets/ico/favicon.ico">
+
+    <title><?php echo APP_TITLE;?></title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet" />
+		<link href="https://cdn.datatables.net/1.10.15/css/jquery.dataTables.min.css" rel="stylesheet" />
+
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <script src="assets/js/modernizr.js"></script>
+
+	    <style>
+	    tr.status200 {
+		    background-color: white;
+	    }
+
+	    tr.status500 {
+		    background-color: red;
+		    color: white;
+	    }
+
+	    tr.status301 {
+		    background-color: orange;
+	    }
+
+	    tr.status302 {
+		    background-color: orange;
+	    }
+
+	    </style>
+  </head>
+
+  <body>
+
+	  <?php require_once('_menu.php'); ?>
+
+	<!-- *****************************************************************************************************************
+	 BLUE WRAP
+	 ***************************************************************************************************************** -->
+	<div id="blue">
+	    <div class="container">
+			<div class="row">
+				<h3>Search Results by tag <b><?php echo (isset($_REQUEST['tag'])?$_REQUEST['tag']:'Not Set?! Whaaat?');?></b>.</h3>
+			</div><!-- /row -->
+	    </div> <!-- /container -->
+	</div><!-- /blue -->
+
+	 <div class="container mtb">
+	 	<div class="row">
+	 		<div class="col-md-12">
+				<?php
+				if (!isset($_REQUEST['tag'])) {
+					?>
+					<h3>Really?</h3>
+					<p>Give me a tag</p>
+					<form method="GET">
+					<input type="text" name="tag" />
+					<button class="btn btn-success">Search</button>
+					</form>
+					<?php
+				} else {
+					?>
+					<table id="tableLinks">
+					<thead>
+						<tr>
+							<td>Host</td>
+							<td>Link</td>
+							<td>Timestamp</td>
+							<?php
+                                                        if ($_SESSION['role'] === 'ADMIN') {
+                                                                echo "<td> </td><td> </td>";
+                                                        }
+                                                        ?>
+                                                </tr>
+					</thead>
+					<tbody>
+					<?php
+					foreach($resultset['rows'] AS $row) {
+						$lst = split('/',  $row[0]);
+						?>
+						<tr id="row<?php echo $row[0];?>">
+							<td>
+								<?php echo justHostName($row[1]); ?></small>
+							</td>
+							<td>
+								<h3><a href="<?php echo $row[1];?>" target="_newWindow"><?php echo urldecode($row[2]);?></a></h3>
+								<small><?php
+								foreach(explode(',', $row[5]) AS $tag) { ?>
+									<span class="badge"><?php echo $tag;?></span>
+								<?php
+								}
+								?>
+							</td>
+							<td>
+								<small><?php echo date('Y-m-d', strtotime($row[4]));?></small>
+							</td>
+							<?php
+							if ($_SESSION['role'] === 'ADMIN') {
+								?><td>
+									<button class="btn btn-danger pull-right" onClick="deleteLink(<?php echo $row[0];?>);">
+										<span class="glyphicon glyphicon-remove"> </span>
+									</button>
+								</td>
+								<td>
+									<a class="btn btn-info" href="linkedit.php?id=<?php echo $row[0];?>" target="_winEditLink">
+										<span class="glyphicon glyphicon-ok"> </span>
+									</a>
+								</td><?php
+							}
+							?>
+						</tr>
+						<?php
+					}?>
+					</tbody>
+					</table><?php
+				}
+				?>
+			</div>
+	 	</div>
+	 </div>
+	 <?php require_once('_footer.php'); ?>
+	 <?php require_once('_scripts.php'); ?>
+	 <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
+	 <script>
+	 $(document).ready(function() {
+    	$('#tableLinks').DataTable({"columns": [
+	null,
+	null,
+	null,
+    { "orderable": false },
+	{ "orderable": false }
+  ]});
+		} );
+	 </script>
+  </body>
+</html>
