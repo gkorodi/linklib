6b28e1d3affd3ba64840dbef4c93b1e82b757411
Gabor Korodi <gabor_korodi@hms.harvard.edu>
2017-08-18 07:22:31 -0400

    Fix host calculation issues

--- list_hosts.php
+++ list_hosts.php
@@ -48,120 +48,67 @@ require_once('_includes.php');
 
 			<?php
 			$keys = Array();
-
 			$linklist = query("SELECT link, status, tags FROM links");
 			foreach($linklist['rows'] AS $row) {
-
 				$urlarr = explode('/', $row[0]);
-
 				$hostname = $urlarr[2];
-
 				if (isset($hostlist[$hostname])) {
 					$hostlist[$hostname]++;
 				} else {
 					$hostlist[$hostname] = 1;
 				}
-
-				if ($row[2] != null && $row[2] != '') {
-					if (isset($hosttlist[$hostname])) {
-						$hosttlist[$hostname]++;
-					} else {
-						$hosttlist[$hostname] = 1;
-					}
-				} else {
-					if (isset($hostntlist[$hostname])) {
-						$hostntlist[$hostname]++;
-					} else {
-						$hostntlist[$hostname] = 1;
-					}
-				}
-
-				$k  = $row[1].'';
-				if (!in_array($k,$keys)) {
-					array_push($keys,$k);
-				}
-
-				if (isset($hostslist[$urlarr[2]])) {
-					$a = $hostslist[$urlarr[2]];
-					if (isset($a[$k])) {
-						$a[$k]++;
-					} else {
-						$a[$k] = 1;
-					}
-					$hostslist[$urlarr[2]] = $a;
-				} else {
-					$hostslist[$urlarr[2]] = Array($k=>1);
-				}
 			}
 			arsort($hostlist);
 			?>
-
-
-		<table class="table">
-		<tr>
-			<th>Host</th>
-			<?php
-			foreach($keys AS $key) {
-				echo "<td colspan='2'>".$key."</td>";
-			}
-			?>
-			<th>Tags</th>
-			<th>NoTags</th>
-			<th>Total</th>
-		</tr>
-
-		<?php
-		foreach($hostlist AS $hname => $hosttotal) {
-      if (isset($_REQUEST['errors']) && !isset($hostntlist[$hname])) {
-        continue;
-      }
-			?>
+			<table class="table">
 			<tr>
-        <th><a href="search_byhost.php?host=<?php echo $hname;?>" target="_newEditLinkWindow"><?php echo $hname;?></a></th>
+				<th>Host</th>
+				<th>Total</th>
+			</tr>
+
 			<?php
-			$statusarr = $hostslist[$hname];
-
-			foreach($keys AS $key) {
-				if (isset($statusarr[$key])) {
-					echo '<td>'.$statusarr[$key].'</td>';
-					echo '<td>'.number_format(($statusarr[$key]/$hosttotal)*100,2).'%'.'</td>';
-				} else {
-					echo '<td></td><td></td>';
-				}
+			foreach($hostlist AS $hname => $hosttotal) {
+	      if (isset($_REQUEST['errors']) && !isset($hostntlist[$hname])) {
+	        continue;
+	      }
+				?>
+				<tr>
+	        <th>
+						<a href="search_byhost.php?host=<?php echo $hname;?>" target="_newEditLinkWindow"><?php echo $hname;?></a>
+					</th>
+					<td>
+						<?php echo $hosttotal;?>
+					</td>
+				</tr>
+				<?php
 			}
-			echo '<td>'.$hosttlist[$hname].'</td>';
-			echo '<td>'.$hostntlist[$hname].'</td>';
+			?>
+			</table>
+		</div>
 
-			echo '<th>'.$hosttotal.'</th>';
-			echo '</tr>';
-		}
-		?>
-		</table>
-        </div>
+		<! -- SIDEBAR -->
+		<div class="col-lg-4">
+			<h4>Search</h4>
+			<div class="hline"></div>
+			<p>
+			<br/><form action="search.php">
+			<input type="text" class="form-control" name="q" placeholder="Search something">
+			</form>
+			</p>
 
-        <! -- SIDEBAR -->
-        <div class="col-lg-4">
-		<h4>Search</h4>
-		<div class="hline"></div>
-		<p>
-		<br/><form action="search.php">
-		<input type="text" class="form-control" name="q" placeholder="Search something">
-		</form>
-		</p>
+			<div class="spacing"></div>
 
-		<div class="spacing"></div>
+			<h4>Statuses</h4>
+			<div class="hline"></div>
 
-		<h4>Statuses</h4>
-		<div class="hline"></div>
+			<div class="spacing"></div>
+			<div id="recent_posts" ></div>
+			<div class="spacing"></div>
 
-		<div class="spacing"></div>
-		<div id="recent_posts" ></div>
-		<div class="spacing"></div>
-
-		<h4>Popular Tags</h4>
-		<div class="hline"></div>
-		<p id="popular_tags">
-		</p>
+			<h4>Popular Tags</h4>
+			<div class="hline"></div>
+			<p id="popular_tags">
+			</p>
 		</div>
 		</div><! --/row -->
 	 </div><! --/container -->
