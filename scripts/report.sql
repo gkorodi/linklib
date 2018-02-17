61b6cdc6e52388ca72c761e3a4d6243c0ee980c4
Gabor Korodi <gabor_korodi@hms.harvard.edu>
2018-02-17 08:53:37 -0500

    Remove some useless stuff

--- scripts/report.sql
+++
@@ -1,7 +0,0 @@
-SELECT count(*) AS total_links FROM links;
-
-SELECT count(*) AS status_count,status FROM links GROUP BY status order by status_count;
-
-SELECT count(*) AS tag_missing_count FROM links WHERE tags IS NULL;
-
-SELECT count(*) AS tag_empty_count FROM links WHERE tags = '';
