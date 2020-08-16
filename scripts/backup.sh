b33acb2b50c25626c74cc9aeaccba36058d37c2d
koRóDi "gabe" GÁbOr <gkorodi@gmail.com>
2020-08-16 08:44:44 -0400

    Small release

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,7 +2,5 @@
 
 if [ ! -d $LINKLIB_HOME/scripts ]; then echo "Cannot find LINKLIB_HOME/scripts directory."; exit 225; fi
 
-/usr/bin/ssh gaborkorodi "mysqldump links links" > links_links_$(date +%Y-%m-%d).sql
-
 /usr/bin/ssh gaborkorodi "mysqldump links" > $LINKLIB_HOME/scripts/data/links_db.sql
 
